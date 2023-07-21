ecfa73b45468e4a9bb22b658a8e27aebe2378e76
emilygknight <emilyknight819@gmail.com>
2023-07-21 18:45:47 -0500

    practice

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 var topics = "HTML, CSS, Git, JavaScript";
 console.log(topics);
+if (0) {
+    console.log("I'm 0!")
+ } else if (null) {
+    console.log("I'm null!")
+ } else if (true) {
+    console.log("I'm true!")
+ }
