2aded20088e058e6083d7f7dddc057277d89f10e
emilygknight <emilyknight819@gmail.com>
2023-07-22 12:50:20 -0500

    error

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ function listTopics() {
 }
 }
 
-function selectTopic(0) {
+function selectTopic() {
 if (topics(0) === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topics(0) === 'CSS') {
