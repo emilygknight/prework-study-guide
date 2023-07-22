4d6aa754a8439f78d70bbcbb8e296d4a02e3de8b
emilygknight <emilyknight819@gmail.com>
2023-07-21 21:51:29 -0500

    saving

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
 for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -13,4 +15,4 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-
+*/
