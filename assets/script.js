fe3c66648cc43f3072fb5e54d27cf59688d26772
emilygknight <emilyknight819@gmail.com>
2023-07-22 12:48:13 -0500

    adding 0

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,17 @@ function listTopics() {
 }
 
 function selectTopic() {
-if (topics === 'HTML') {
+if (topics(0) === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (topics(0) === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (topics(0) === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (topics(0) === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
 }
 listTopics()
-selectTopic(0)
+selectTopic()
