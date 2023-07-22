c606baa18b970892c7549177662d1f625bf43829
emilygknight <emilyknight819@gmail.com>
2023-07-22 12:38:27 -0500

    changed to topics

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,23 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-function listTopics() {
+function listTopicss() {
   for (var x = 0; x < topics.length; x++) {
   console.log(topics[x]);
 }
 }
 
-function selectTopic() {
-if (topic === 'HTML') {
+function selectTopics() {
+if (topics === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (topics === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (topics === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (topics === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
 }
 listTopics()
+selectTopics()
