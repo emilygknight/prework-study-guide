169b8bd1bda09e5dd2c47b61985e66cc80822b14
emilygknight <emilyknight819@gmail.com>
2023-07-22 12:52:07 -0500

    [ not (

--- assets/script.js
+++ assets/script.js
@@ -7,13 +7,13 @@ function listTopics() {
 }
 
 function selectTopic() {
-if (topics(0) === 'HTML') {
+if (topics[0] === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics(0) === 'CSS') {
+} else if (topics[0] === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics(0) === 'Git') {
+} else if (topics[0] === 'Git') {
   console.log("Let's study Git!");
-} else if (topics(0) === 'JavaScript') {
+} else if (topics[0] === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
